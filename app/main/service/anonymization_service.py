483ad38182a7c843bfa766304602c1ef2a0a6531
Michael Silva <michael.maicom0@gmail.com>
2023-05-24 03:38:05 -0300

    refactor(anonymization): change save and delete code comments

--- app/main/service/anonymization_service.py
+++ app/main/service/anonymization_service.py
@@ -41,30 +41,33 @@ anonymization_mapping = {
 
 
 def save_new_anonymization(table_id: int) -> None:
-    # Get the table object with the specified ID, including the associated database
+    # Get the table object with the specified ID, including the associated database information
     table = get_table(table_id=table_id, options=[joinedload(AnonTable.database)])
 
     try:
+        # Create an engine based on the cloud URL of the table's database
         engine = create_engine(url=table.database.cloud_url)
     finally:
+        # Check if the engine has the table, and if so, raise an exception
         if inspect(engine).has_table(table.name):
             raise DefaultException("table_already_anonymized", code=409)
         engine.dispose()
 
-    # Clone the database and create a new table with the same structure
+    # Clone the database by creating a destination table with the same structure and columns
     clone_database(
         database=table.database,
         src_table=table.name,
         dest_columns=[table.primary_key] + [column.name for column in table.columns],
     )
 
-    # Create an engine to connect to the database and reflect existing columns
+    # Create a new engine based on the URL of the table's database
     engine = create_engine(url=table.database.url)
     metadata = MetaData()
 
+    # Get the primary key of the table
     primary_key = table.primary_key
 
-    # Create a table object representing the old table structure
+    # Create a table object for the table, including the primary key and other columns
     tableObj = Table(
         table.name,
         metadata,
@@ -73,15 +76,16 @@ def save_new_anonymization(table_id: int) -> None:
         autoload_with=engine,
     )
 
+    # Create a dictionary mapping column names to column indexes for the table
     column_indexes = {
         column_name: index for index, column_name in enumerate(tableObj.columns.keys())
     }
 
-    # Create a session
+    # Create a session for the engine
     session = Session(bind=engine)
 
     try:
-        # Select all rows from the old table
+        # Retrieve the results from the table in batches
         results = session.execute(
             tableObj.select().order_by(tableObj.columns[table.primary_key])
         )
@@ -95,28 +99,29 @@ def save_new_anonymization(table_id: int) -> None:
             for row in rows:
                 values = {}
                 values["_" + primary_key] = row[0]
-                # Generate anonymized values for each column in the table
+
+                # Anonymize the values of each column in the row
                 for column in table.columns:
+                    # Seed the faker instance with a specific value based on the database, table, column, and row
                     faker.seed_instance(
                         f"database{table.database_id}table{table_id}column{column.name}row{row[0]}value{str(row[column_indexes[column.name]])}"
                     )
+                    # Apply the anonymization mapping function for the column's anonymization type
                     values[column.name] = anonymization_mapping.get(
                         column.anonymization_type
                     )()
                 rows_to_update.append(values)
 
-            # Update the corresponding rows in the new table with the anonymized values
+            # Update the corresponding rows in the table using the anonymized values
             session.execute(
                 tableObj.update().where(text(f"{primary_key} = :_{primary_key}")),
                 rows_to_update,
             )
             session.commit()
 
-            # Fetch the next batch of rows
             rows = results.fetchmany(batch_size)
 
     except Exception as e:
-        # Print any exceptions that occur during the process
         print(e)
 
     finally:
@@ -128,55 +133,57 @@ def save_new_anonymization(table_id: int) -> None:
 
 
 def delete_anonymization(table_id: int):
-    # Get the table object with the specified ID, including the associated database
+    # Get the table object with the specified ID, including the associated database information
     table = get_table(table_id=table_id, options=[joinedload(AnonTable.database)])
 
+    # Get the primary key of the table
     primary_key = table.primary_key
 
-    # Create an engine and metadata for the source database
     try:
+        # Create a source engine based on the cloud URL of the table's database
         src_engine = create_engine(url=table.database.cloud_url)
     except exc.OperationalError:
         raise DefaultException("cloud_database_not_exists", code=409)
 
+    # Check if the source engine has the table
     if not inspect(src_engine).has_table(table.name):
         raise DefaultException("table_not_anonymized", code=409)
 
+    # Create metadata and table objects for the source table
     src_metadata = MetaData()
-
-    # Reflect the structure of the source table
     src_table = Table(
         table.name,
         src_metadata,
         autoload_with=src_engine,
     )
 
-    # Create a session
+    # Create a session for the source engine
     src_session = Session(bind=src_engine)
 
+    # Create a dictionary mapping column indices to column names for the source table
     column_names = {
         index: column_name for index, column_name in enumerate(src_table.columns.keys())
     }
 
-    # Create an engine and metadata for the destination database
     try:
+        # Create a destination engine based on the URL of the table's database
         dest_engine = create_engine(url=table.database.url)
     except exc.OperationalError:
         raise DefaultException("database_not_exists", code=409)
 
+    # Check if the destination engine has the table
     if not inspect(dest_engine).has_table(table.name):
         raise DefaultException("table_not_exists", code=409)
 
+    # Create metadata and table objects for the destination table
     dest_metadata = MetaData()
-
-    # Create a table object representing the destination table
     dest_table = Table(table.name, dest_metadata, autoload_with=src_engine)
 
-    # Create a session
+    # Create a session for the destination engine
     dest_session = Session(bind=dest_engine)
 
     try:
-        # Select all rows from the source table
+        # Retrieve the results from the source table in batches
         results = src_session.execute(
             src_table.select().order_by(src_table.columns[table.primary_key])
         )
@@ -189,7 +196,8 @@ def delete_anonymization(table_id: int):
             rows_to_update = []
             for row in rows:
                 values = {}
-                # Prepare the values for the update statement
+
+                # Iterate over the columns of the row and construct a dictionary of values
                 for i, value in enumerate(row):
                     if i == 0:
                         values["_" + column_names[i]] = value
@@ -197,14 +205,13 @@ def delete_anonymization(table_id: int):
                         values[column_names[i]] = value
                 rows_to_update.append(values)
 
-            # Update the corresponding rows in the new table with the anonymized values
+            # Update the corresponding rows in the destination table using the constructed values
             dest_session.execute(
                 dest_table.update().where(text(f"{primary_key} = :_{primary_key}")),
                 rows_to_update,
             )
             dest_session.commit()
 
-            # Fetch the next batch of rows
             rows = results.fetchmany(batch_size)
 
     except exc.OperationalError:
@@ -212,16 +219,18 @@ def delete_anonymization(table_id: int):
     except exc.NoSuchTableError:
         raise DefaultException("table_not_exists", code=409)
     except Exception as e:
-        # Print any exceptions that occur during the process
         print(e)
     finally:
-        # Close the session
+        # Close the source session
         src_session.close()
 
+        # Drop the source table from the source engine
         src_table.drop(bind=src_engine, checkfirst=True)
 
+        # Close the destination session
         dest_session.close()
-        # Dispose of the engines
+
+        # Dispose the source and destination engines
         src_engine.dispose()
         dest_engine.dispose()
 
